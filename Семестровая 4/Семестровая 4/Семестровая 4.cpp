21daa26e8e34d43b1dd918d3bac4e52d53ea6c4e
LittleTimur <167896566+LittleTimur@users.noreply.github.com>
2024-12-10 19:10:39 +0300

    Add files via upload

---
+++ Семестровая 4/Семестровая 4/Семестровая 4.cpp
@@ -0,0 +1,297 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+void Show(ifstream& fin, ofstream& fout)
+{
+    fout << "Входные данные:" << endl;
+    char x;
+    while (fin >> x)
+    {
+        fout << x;
+    }
+    fout << endl << endl;
+}
+void ShowMistake(ofstream& f, int k)
+{
+    if (k == 0 || k == 18)
+        f << "Ошибка. Ожидался символ: 'A'...'Z'";
+    else if (k == 1)
+        f << "Ошибка. Ожидался символ: 'A'...'Z' или '0'...'9'";
+    else if (k == 2 || k == 14)
+        f << "Ошибка. Ожидался символ: ':'";
+    else if (k == 3 || k == 15)
+        f << "Ошибка. Ожидался символ: '='";
+    else if (k == 16)
+        f << "Ошибка. Ожидался символ: '!', или '%', или '^'";
+    else if (k >= 4 && k <= 11)
+        f << "Ошибка. Ожидался символ: '0' или '1'";
+    else if (k == 17)
+        f << "Ошибка. Ожидался символ: 'A'...'Z', или '0', или '1'";
+    else if (k == 12)
+        f << "Ошибка. Ожидался символ: ';'";
+    else if (k == 13)
+        f << "Ошибка. Ожидался символ: 'A'...'Z' или ' '";
+
+}
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    Show(fin, fout);
+    fin.clear();
+    fin.seekg(0);
+    char x;
+    int q = 0, k = 0;
+    fout << "Выходные данные:" << endl;
+    while (fin.peek() != EOF && q != 20)
+    {
+        fin.get(x);
+        switch (q)
+        {
+        case 0:
+            if (x >= 'A' && x <= 'Z')
+            {
+                fout << x;
+                q = 1;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 1:
+            if (x >= 'A' && x <= 'Z')
+            {
+                fout << x;
+                q = 2;
+            }
+            else if (x >= '0' && x <= '9')
+            {
+                fout << x;
+                q = 14;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 2:
+            if (x == ':')
+            {
+                fout << x;
+                q = 3;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 3:
+            if (x == '=')
+            {
+                fout << x;
+                q = 4;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 4:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 5;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 5:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 6;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 6:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 7;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 7:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 8;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 8:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 9;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 9:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 10;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 10:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 11;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 11:
+            if (x == '1' || x == '0')
+            {
+                fout << x;
+                q = 12;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 12:
+            if (x == ';')
+            {
+                fout << x;
+                q = 13;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 13:
+            if (x >= 'A' && x <= 'Z')
+            {
+                fout << x;
+                q = 1;
+            }
+            else if (x == ' ')
+                q = 19;
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 14:
+            if (x == ':')
+            {
+                fout << x;
+                q = 15;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 15:
+            if (x == '=')
+            {
+                fout << x;
+                q = 16;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 16:
+            if (x == '!' || x == '%' || x == '^')
+            {
+                fout << x;
+                q = 17;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 17:
+            if (x >= 'A' && x <= 'Z')
+            {
+                fout << x;
+                q = 18;
+            }
+            else if (x == '1' || x == '0')
+                q = 5;
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        case 18:
+            if (x >= 'A' && x <= 'Z')
+            {
+                fout << x;
+                q = 12;
+            }
+            else
+            {
+                k = q;
+                q = 20;
+            }
+            break;
+        }
+    }
+    fout << endl;
+    if (q == 19)
+        fout << "Ошибок не выявлено";
+    else
+        ShowMistake(fout, k);
+    return 0;
+}
---
+++ семестровая 3/семестровая 3/семестровая 3.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+const int Indent = 7;
+const int Lenght = 97;
+struct Road
+{
+    int len = 0;
+    char color = 'g';// green/yellow/red
+};
+struct node
+{
+    int data;
+    node* next;
+};
+Road** Make(int);
+void Create_tabl(ifstream&, Road**&);
+void Create_mass(int*, int);
+void push(node*&, int);
+int Find_Color_Road(Road**&, int, int, char);
+void Change_color(Road**&, int, int, char);
+void FindRoad(Road**&, node*&, int*, int, int, int);
+void pop(node*&, int&);
+bool empty_list(node*);
+int Distance(node*&, Road**);
+void star(ofstream&);
+void star1(ofstream&);
+void Show(ofstream&, Road**, int, int, int);
+void Show(ofstream&, node*);
+void Show(ofstream&, int);
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    int x, y, n; // n - кол-во вершин
+    fin >> n >> x >> y;
+    Road** road = Make(n);
+    Create_tabl(fin, road);
+    Show(fout, road, n, x, y);
+    int* age = new int[n]; // 1- новый, 0 - старый
+    Create_mass(age, n);
+    age[x - 1] = 0;
+    node* top = NULL;
+    FindRoad(road, top, age, n, x - 1, y - 1);
+    Show(fout, top);
+    int Lenght = Distance(top, road);
+    Show(fout, Lenght);
+    system("pause");
+    return 0;
+}
+Road** Make(int n)
+{
+    Road** x = new Road * [n];
+    for (int i = 0; i < n; i++)
+        x[i] = new Road[n];
+    return x;
+}
+void Create_tabl(ifstream& f, Road**& x)
+{
+    int i, j, l;
+    while(!f.eof())
+    {
+        f >> i >> j >> l;
+        x[i - 1][j - 1].len = l;
+        x[j - 1][i - 1].len = l;
+    }
+}
+void Create_mass(int* x, int n)
+{
+    for (int i = 0; i < n; i++)
+        x[i] = 1;
+}
+void push(node*& top, int data)
+{
+    node* p;
+    p = new node;
+    p->data = data;
+    p->next = top;
+    top = p;
+}
+int Find_Color_Road(Road**& road, int n, int x, char a)
+{
+    int x1 = -1;
+    bool b = false;
+    for (int i = 0; i < n && !b; i++)
+        if (road[i][x].len != 0 && road[i][x].color == a)
+        {
+            x1 = i;
+            b = true;
+        }
+    return x1;
+}
+void Change_color(Road**& road, int i, int j, char a)
+{
+    road[i][j].color = a;
+    road[j][i].color = a;
+}
+void FindRoad(Road**& road, node*& top, int* age, int n, int x, int y)
+{
+    int x1 = x;
+    bool find = false;
+    while (!find)
+    {
+        push(top, x1);
+        x = x1;
+        x1 = Find_Color_Road(road, n, x, 'g');
+        if (x1 == -1)
+        {
+            x1 = top->next->data;
+            if (road[x1][x].color == 'r')
+                x1 = Find_Color_Road(road, n, x, 'y');
+            Change_color(road, x, x1, 'r');
+        }
+        else if (x1 == y)
+        {
+            push(top, x1);
+            find = true;
+        }
+        else if (age[x1] && road[x][x1].color == 'g')
+        {
+            Change_color(road, x, x1, 'y');
+            age[x1] = 0;
+        }
+        else if (!age[x1] && road[x][x1].color == 'g')
+        {
+            Change_color(road, x, x1, 'r');
+            push(top, x1);
+            x1 = x;
+        }
+    }
+}
+void pop(node*& top, int& data)
+{
+    node* p;
+    data = top->data;
+    p = top->next;
+    delete top;
+    top = p;
+}
+bool empty_list(node* top)
+{
+    return top == NULL;
+}
+int Distance(node*& top, Road** road)
+{
+    node* p;
+    int data1, data2, len = 0;
+    pop(top, data1);
+    while (!empty_list(top))
+    {
+        data2 = data1;
+        pop(top, data1);
+        len += road[data1][data2].len;
+    }
+    return len;
+}
+void star(ofstream& f)
+{
+    f.width(Indent + 1);
+    for (int i = 0; i < Lenght; i++)
+        f << "*";
+    f << endl;
+}
+void star1(ofstream& f)
+{
+    f.width(Indent + 1);
+    f << "*";
+    for (int i = 0; i < 16; i++)
+    {
+        f.width(6);
+        f << "*";
+    }
+    f << endl;
+}
+void Show(ofstream& f, Road** a, int n, int x, int y)
+{
+    f << "Поиск путей в схеме дорог" << endl;
+    f << "Входные данные:" << endl;
+    f << "n = " << n << endl;
+    f << "x = " << x << endl;
+    f << "y = " << y << endl;
+    f << "Матрица смежности:" << endl;
+    f.width(Indent + 4);
+    for (int i = 0; i < n; i++)
+    {
+        f << i + 1;
+        f.width(6);
+    }
+    f << endl;
+    star(f);
+    for (int i = 0; i < n; i++)
+    {
+        star1(f);
+        f.width(Indent - 1);
+        f << i + 1 << " ";
+        f << "*";
+        for (int j = 0; j < n; j++)
+        {
+            f.width(3);
+            f << a[i][j].len;
+            f.width(3);
+            f << "*";
+        }
+        f << endl;
+        star1(f);
+        star(f);
+    }
+}
+void Show(ofstream& f, node* top)
+{
+    f << "Пройденный путь:" << endl;
+    f.width(Indent + 1);
+    node* p = top;
+    while (p)
+    {
+        f << p->data + 1 << " ";
+        p = p->next;
+    }
+    f << endl;
+}
+void Show(ofstream& f, int lenght)
+{
+    f << "Пройденное расстояние: " << lenght << endl;
+}
